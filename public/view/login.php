0816c781c8c45fe717bb9a1d2a93d9b4f136398e
Samweli <smwltwesa6@gmail.com>
2014-03-23 02:32:02 +0300

    changed the index page

--- public/view/login.php
+++ public/view/login.php
@@ -1,5 +1,5 @@
 <?php //require_once('../includes/helper/initialize.php'); 
-        require_once('../includes/helper/functions.php');
+        require_once('../../includes/helper/functions.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,8 +7,8 @@
 <meta charset="UTF-8">
 <title>Login|UICT Community</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
-<link href="css/custom.css" type="text/css" rel="stylesheet">
+<link href="../css/bootstrap.min.css" type="text/css" rel="stylesheet">
+<link href="../css/custom.css" type="text/css" rel="stylesheet">
 </head>
 <body>
    <section id="login_section">
@@ -20,7 +20,7 @@
            }
         ?>
      </div>
-     <form name="login" action="../includes/controller/process_login.php" method="post">
+     <form name="login" action="../../includes/controller/process_login.php" method="post">
        <label for="reg_number"></label>
        <input type="text" name="reg_number" id="reg_number" required placeholder="Registration number">
        <label for="password"></label>
@@ -30,7 +30,7 @@
        <span><a href="reset_password.php">Forgot password?</a></span>
      </form>
    </section>
-   <script src="js/jquery_min.js"></script>
-   <script src="js/bootstrap.min.js"></script>
+   <script src="../js/jquery_min.js"></script>
+   <script src="../js/bootstrap.min.js"></script>
 </body>
 </html>
