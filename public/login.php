5de32846cba23db1326c427a3411adf729eac107
Mickamwamba <kimollomick17@gmail.com>
2014-03-23 15:36:48 +0300

    background image

--- public/login.php
+++ public/login.php
@@ -10,7 +10,8 @@
 <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
 <link href="css/custom.css" type="text/css" rel="stylesheet">
 </head>
-<body>
+
+ <body>   
    <section id="login_section">
      <div class="login_header"><img src="img/uict.jpg"></div>
      <div class="message">
@@ -20,16 +21,19 @@
            }
         ?>
      </div>
-     <form name="login" action="../includes/controller/process_login.php" method="post">
+     <form name="login" action="../includes/controller/process_login.php" method="post" >
+	 <fieldset>
        <label for="reg_number"></label>
-       <input type="text" name="reg_number" id="reg_number" required placeholder="Registration number">
+       <input type="text" name="reg_number" id="reg_number" required placeholder="Registration number" size="24"> <!--the size of the box is altered here!-->
        <label for="password"></label>
-       <input type="password" name="password" id="password" required placeholder="Password">
+       <input type="password" name="password" id="password" required placeholder="Password" size ="24"> <!--the size of the box is changed here-->
        <label for="login"></label>
        <input type="submit" value="Login" id="login" class="btn btn-info">
        <span><a href="reset_password.php">Forgot password?</a></span>
+	   </fieldset>
      </form>
    </section>
+
    <script src="js/jquery_min.js"></script>
    <script src="js/bootstrap.min.js"></script>
 </body>
