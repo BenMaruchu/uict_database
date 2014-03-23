755dbee918c9f788f416bf6fe4a56d79091fd439
jacquline <jacquelinemassawe178@gmail.com>
2014-03-23 18:26:18 +0300

    added event page

---
+++ public/view/EventsForm.php
@@ -0,0 +1,162 @@
+
+<!DOCTYPE html>
+    <html lang='en'>
+        <head>
+            <meta charset="utf-8" />
+            <title>EventsForm</title>
+            <link href="../css/bootstrap.min.css" type="text/css" rel="stylesheet"> 
+            <link href="../css/bootstrap-theme.css" type="text/css" rel="stylesheet"> 
+
+            <link rel="stylesheet" type="text/css" href="../css/style.css" />
+            <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine" />
+            <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Gafata" />
+            <link rel="stylesheet" type="text/css" href="../css/main.css"/>
+            
+        </head>
+        <body>
+            <div id="page">
+			<header class="navbar navbar-static-top bs-docs-nav u_header" id="top" role="banner">
+              <div class="container">
+                 <div class="navbar-header">
+     
+                 <a href="/index.php" class="navbar-brand"><img class="logo img col-sm-4" src="../img/uict.jpg" alt="uict logo"
+		/></a>   	
+                </div>
+            <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+               
+                     <ul class="nav navbar-nav navbar-right">
+                            <li class="active" ><a href="#home">Home</a></li>
+                            <li class="list"><a href="#projects">Projects</a></li>
+                            <li class="list"><a href="#events">Events</a></li>
+                            <li class="list"><a href="#about">About</a></li>
+                            <li class="list"><a href="login.php">Sign In</a></li>
+                            <li ><a><i  title="click to search" data-toggle="dropdown"
+				class="dropdown-toggle glyphicon glyphicon-search"></i>
+                                </a>
+                            </li>
+                     </ul>
+                </nav>
+               </div>
+         <div class="for_search">
+                       <div class="container">
+                         <div class="btn-group pull-right" >
+                        <div id="search">
+                            <form action="search.php" method="get">
+                                <ul class="nav dropdown-menu" role="menu">
+                                <li><input class="text_"type="text" name="searchQuery" alt="forSearch" id="searchQuery"  
+				     placeholder="search"/></li>
+                                <li><input class="btn " type="submit" name="submit" id="submit" value="submit" /></li>
+				</ul>
+                            </form>
+                        </div>
+                        </div>
+			</div>
+           </div>
+            </header>
+			<div class="container">
+			<div class="row ">
+                    <!-- u_header -->
+                      <div id="bannerForRegistration">
+                            <div class="message">
+                                <span>Events Page </span>
+                            </div>
+                            
+                        </div>
+						</div>
+                        <!-- banner -->
+                      <!-- header -->
+                     <div class="row u_row">
+					 <div class="content">
+                        <div class="description" >
+                            <div class="u_heading">
+                            <h4>DESCRIPTION</h4>
+                            </div>
+                            <p>There are a lot of exciting things to see and do in UICT COMMUNITY....
+							It is a beautiful Community with different events and visual journey of the UICT days
+							Stay tuned for more interesting and inspiring upcoming events... 
+                           
+                            </p>
+                            
+                           
+					</div>
+					</div>
+					</div>
+					
+				<div class="row u_row" >
+                            <div class="col-md-3">
+                             
+                              <img  src="../img/bibo.jpg" alt="event image"  width="225" height="150"/> 
+                              <p></p>
+                              
+                            </div>
+                        
+                           <div class="col-md-3">
+                              <img  src="../img/nkurumah.jpg" alt="event image"  width="225" height="150"/>
+                              </div>
+							
+                        <div class="col-md-3">
+                              <img  src="../img/chuo.jpg" alt="event image"  width="225" height="150"/>
+                             </div>
+                        
+                         <div class="col-md-3">
+                              <img  src="../img/mabibo.jpg" alt="event image"  width="225" height="150"/>
+                          </div>
+						  
+                      </div>
+					  
+					  <div class="row u_row" >
+                            <div class="col-md-3">
+                              <img  src="../img/biboo.jpg" alt="event image"  width="225" height="150"/> 
+                             </div>
+                        
+                          <div class="col-md-3">
+                             <img  src="../img/hall2.jpg" alt="event image"  width="225" height="150"/>
+                           </div>
+							
+                       <div class="col-md-3">
+                              <img  src="../img/ucc.jpg" alt="event image"  width="225" height="150"/>
+                        </div>
+                        
+                         <div class="col-md-3">
+                             <img  src="../img/library.jpg" alt="event image"  width="225" height="150"/>
+                           </div>
+                      </div>
+              
+				<div class="content">
+                      <div class="u_footer">
+                        <div class="footer_content">
+                        <div class="u_social">
+                            <ul class=" nav u_footer_nav">
+                                <li><a href="www.facebook.com/uict" > <img src="../img/fb-icon.jpg" alt="facebook logo" width="20" height="20" /> Facebook </a></li>
+                                 <li><a href="www.twitter.com/uict" ><img src="../img/twitter-icon.jpg" alt="twitter logo" width="20" height="20" /> Twitter </a></li>
+                                  <li><a href="www.google+.com/uict" ><img src="../img/google.jpg" alt="google+ logo"  width="20" height="20"/> Google+ </a></li>
+                            </ul>
+                        </div>
+                        <div class="container">
+                        <div class="u_links">
+                            <ul class="nav u_footer_nav">
+                                <li><a href="#about" >About</a></li>
+                                <li><a href="#terms" >Terms</a></li>
+                                <li><a href="#privacy">Privacy</a></li>
+                            </ul>
+                        </div>
+                        <div class="u_links">
+                            <ul class="nav u_footer_nav">
+                                <li><a href="#events" >Events</a></li>
+                                <li><a href="#projects" >Project</a></li>
+                                <li><a href="#contacts" >Contacts</a></li>
+                            </ul>
+                        </div>
+                        </div>
+                        </div>
+                        <div class="container">
+                        <div class="nav pull-right">
+                            <p>&copy;2014 UICT Community</p>
+                        </div>
+                        </div>
+                        
+                      </div>
+                    </div>
+            </div>
+        </body>
+    </html>
