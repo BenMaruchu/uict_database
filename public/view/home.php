9f9f03e8232cd65d9467978db49cae5e9513d1c0
Samweli <smwltwesa6@gmail.com>
2014-03-23 19:14:03 +0300

    added fixture in homepage

--- public/view/home.php
+++ public/view/home.php
@@ -65,23 +65,38 @@
 			    <div class="col-lg-6">
 				   <div class="row ">
 					  
-					  <div class="col-lg-3 offset">
+					  <div class="col-lg-3">
 						 <img class="img img-thumbnail" src="../img/profile_photo.jpg" />
 					  <a href="profile.php" title="Checkout Profile" >Samweli Twesa</a>
 					  </div>
 					  <div class="col-lg-6 col-md-offset-3">
 						 <form action="../controller/search.php" method="get">
 							<div class="input-group">
-							<input type="text" class="form-control-min" size="40"  placeholder="Search a colleague"  />
+							<input type="text" class="form-control-min" size="40" placeholder="Search a colleague"  />
 							<span class="input-group-btn">
 						        <input type="submit" class="btn btn-primary" value="Search" />
 							</span>
 							</div>
 						 </form>
+						 
 					  </div>
+					  <div class="container">
+					<div class="col-lg-3 pull-right recent">
+						 <div class="recent_activity">
+						  <h3>Recent Activities</h3> 
+						 </div>
+						 <ul class="nav">
+							<li class="activity_li">Today: <a>Shared OOP book</a></li>
+							<li class="activity_li">Yesterday: <a>Joined Project Logo</a></li>
+							<li class="activity_li">Yesterday: <a>Commented on Charity Event</a></li>
+							<li class="activity_li">Last Week: <a>Leaved Project Together</a></li>
+							
+						 </ul>
+					  </div>
+					</div>
 					
 				   </div>
-				   <div class="row u_row">
+				   <div class="row u_row lower_row">
 					<div class="col-lg-6">
 					  <ul class="nav navigation-menu" >
 						 <li  class="active-menu list-menu"><a><i class="diff glyphicon glyphicon-pushpin"></i> Enrolled Projects</a></li>
@@ -89,6 +104,76 @@
 						 <li  class="list-menu"><a><i class="diff glyphicon glyphicon-map-marker"></i> Enrolled Projects</a></li>
 						 <li  class="list-menu"><a><i class="diff glyphicon glyphicon-bookmark"></i> Enrolled Projects</a></li>
 					  </ul>
+					</div>
+					<div class="col-md-offset-6">
+					  <div class="main_content">
+						 <div class="a_content">
+					            
+						   <h3><span><img src="../img/leader.jpg" class="img col-sm-2" title="Project Leader"/></span>Project heading</h3>
+						   <p>As a response to the inherent need for plore technological challenges embedded with innovation opportunities, UICT Community serves as a pla, University Information and Communication Technologies Community.The community engages in various
+						   </p>
+						   <ul class="nav navbar-nav">
+							<li class="links_project"><a href="../controller/information.php" ><i class=" diff glyphicon glyphicon-envelope"> </i>  Inform a friend</a></li>
+							<li class="links_project"><a ><i class="diff glyphicon glyphicon-trash"></i>  Leave Project</a></li>
+							<li class="links_project pull-right"><a> Read More</a></li>
+						   </ul>
+						  
+						 </div>
+	                                  
+						 <div class="a_content">
+					            
+						   <h3><span><img src="../img/jack.jpg" class="img col-sm-2" title="Project Leader"/></span>Project heading</h3>
+						   <p>As a response to the inherent need for plore technological challenges embedded with innovation opportunities, UICT Community serves as a pla, University Information and Communication Technologies Community.The community engages in various
+						   </p>
+						   <ul class="nav navbar-nav">
+							<li class="links_project"><a href="../controller/information.php" ><i class=" diff glyphicon glyphicon-envelope"> </i>  Inform a friend</a></li>
+							<li class="links_project"><a ><i class="diff glyphicon glyphicon-trash"></i>  Leave Project</a></li>
+							<li class="links_project pull-right"><a> Read More</a></li>
+						   </ul>
+						  
+						 </div>
+	                                  
+						 <div class="a_content">
+					            
+						   <h3><span><img src="../img/leader.jpg" class="img col-sm-2" title="Project Leader"/></span>Project heading</h3>
+						   <p>As a response to the inherent need for plore technological challenges embedded with innovation opportunities, UICT Community serves as a pla, University Information and Communication Technologies Community.The community engages in various
+						   </p>
+						   <ul class="nav navbar-nav">
+							<li class="links_project"><a href="../controller/information.php" ><i class=" diff glyphicon glyphicon-envelope"> </i>  Inform a friend</a></li>
+							<li class="links_project"><a ><i class="diff glyphicon glyphicon-trash"></i>  Leave Project</a></li>
+							<li class="links_project pull-right"><a> Read More</a></li>
+						   </ul>
+						  
+						 </div>
+	                                  
+						 <div class="a_content">
+					            
+						   <h3><span><img src="../img/jack.jpg" class="img col-sm-2" title="Project Leader"/></span>Project heading</h3>
+						   <p>As a response to the inherent need for plore technological challenges embedded with innovation opportunities, UICT Community serves as a pla, University Information and Communication Technologies Community.The community engages in various
+						   </p>
+						   <ul class="nav navbar-nav">
+							<li class="links_project"><a href="../controller/information.php" ><i class=" diff glyphicon glyphicon-envelope"> </i>  Inform a friend</a></li>
+							<li class="links_project"><a ><i class="diff glyphicon glyphicon-trash"></i>  Leave Project</a></li>
+							<li class="links_project pull-right"><a> Read More</a></li>
+						   </ul>
+						  
+						 </div>
+	                                
+						 <div class="a_content">
+					            
+						   <h3><span><img src="../img/leader.jpg" class="img col-sm-2" title="Project Leader"/></span>Project heading</h3>
+						   <p>As a response to the inherent need for plore technological challenges embedded with innovation opportunities, UICT Community serves as a pla, University Information and Communication Technologies Community.The community engages in various
+						   </p>
+						   <ul class="nav navbar-nav">
+							<li class="links_project"><a href="../controller/information.php" ><i class=" diff glyphicon glyphicon-envelope"> </i>  Inform a friend</a></li>
+							<li class="links_project"><a ><i class="diff glyphicon glyphicon-trash"></i>  Leave Project</a></li>
+							<li class="links_project pull-right"><a> Read More</a></li>
+						   </ul>
+						  
+						 </div>
+	                                  </div>
+					  </div>
+					  <!-- end u_main_content -->
 					</div>
 				   </div>
 			    </div>
