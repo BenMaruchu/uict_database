20eaf000f6b210c186feec4ee37890adec99d769
Samweli <smwltwesa6@gmail.com>
2014-03-28 23:26:39 +0300

    added template engine

--- includes/controller/process_logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-   if(urldecode($_GET['logout']) == true){
-      require_once('../model/session.php');
-      require_once('../helper/functions.php');
-      $session->logout(true);
-      redirect('../../public/login.php');
-   }
-?>
--- includes/helper/initialize.php
+++
@@ -1,5 +0,0 @@
-<?php
-   require_once('../model/user.php');
-   require_once('../model/database.php');
-   require_once('functions.php');
-?>
